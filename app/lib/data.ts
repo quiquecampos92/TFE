2d61c925f2c97a1a0ce80b98eabc65215660124c
“Quique” <quiquecampos92@outlook.com>
2024-02-19 23:20:46 +0100

    tercer commit con archivos comentados

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -182,6 +182,24 @@ export async function fetchMovimientosPages() {
   }
 }
 
+// export async function fetchCustomers() {
+//   try {
+//     const data = await sql<CustomerField>`
+//       SELECT
+//         id,
+//         name
+//       FROM customers
+//       ORDER BY name ASC
+//     `;
+
+//     const customers = data.rows;
+//     return customers;
+//   } catch (err) {
+//     console.error('Database Error:', err);
+//     throw new Error('Failed to fetch all customers.');
+//   }
+// }
+
 export async function fetchMovimientoById(id: string) {
   noStore();
   try {
