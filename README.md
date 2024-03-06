ec5ca3fa30fc3a726ed53b82763f726409b2688d
“Quique” <quiquecampos92@outlook.com>
2024-03-06 19:15:56 +0100

    prueba añadir ruta estadisticas

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## Next.js App Router Course - Final
-
+NPM INSTALL para instalar la carpeta node modules
+env.example crear y explicar
 This is the final template for the Next.js App Router Course. It contains the final code for the dashboard application.
+
+QR link a la web
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
--- app/ui/moneyhub-logo.tsx
+++ app/ui/moneyhub-logo.tsx
@@ -1,13 +1,18 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-export default function AcmeLogo() {
+export default function MoneyHubLogo() {
   return (
     <>
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
       >
+      <Image
+        src="/public/moneyhub-icon.png"
+        alt="MoneyHub Logo"
+        width={20}
+        height={20}
+      />
       <p className="text-[40px] ">MoneyHub</p>
     </div>
       </>
